480355a8498a55aa5265a733a61d4affbf9f8d44
sanzona <twonapom@gmail.com>
2020-05-12 13:58:44 +0800

    添加插件

--- docs/_coverpage.md
+++ docs/_coverpage.md
@@ -1,4 +1,3 @@
-<!-- <img width="180px" style="border-radius: 50%" bor src="https://nodejsred.oss-cn-shanghai.aliyuncs.com/nodejs_roadmap-logo.jpeg?x-oss-process=style/may"> -->
 ![avatar](img/sanzo.png ':size=200%')
 
 # Sanzo
--- docs/js/docsify.js
+++ docs/js/docsify.js
@@ -4,11 +4,49 @@ window.$docsify = {
   coverpage: true, // 首页
   loadNavbar: false, // 导航栏
   loadSidebar: true, // 侧边栏
-	subMaxLevel: 5, // 目录级别
+  subMaxLevel: 5, // 目录级别
 
 
   alias: { // 嵌套的侧边栏
     './post/_sidebar.md': './_sidebar.md',
     '.*/_navbar.md': './_navbar.md'
   },
+
+
+  // 完整配置参数
+  search: {
+    maxAge: 86400000, // 过期时间，单位毫秒，默认一天
+    paths: 'auto', // or 'auto'
+    placeholder: '搜索',
+    noData: '找不到文件',
+    // 搜索标题的最大层级, 1 - 6
+    depth: 4,
+  },
+
+
+  // 黑夜模式
+  darkMode: {
+    dark: {
+      background: "#1c2022",
+      toggleBtnBg: "#34495e",
+      textColor: "#b4b4b4"
+    },
+    light: {
+      background: "white",
+      toggleBtnBg: "green",
+      textColor: "var(--theme-color)"
+    }
+  },
+
+  // 字数统计
+  count:{
+    countable: false,
+    fontsize: '0.8em',
+    color: 'rgb(90,90,90)',
+    language: 'chinese',
+    position: "top",
+    float: "left",
+    isExpected: true, // 时间
+  },
+
 }
